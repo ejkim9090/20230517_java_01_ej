c3b14c70052a7df5b80e17968d98c3899a14d2db
ejkim9090 <ej.kh.kim@gmail.com>
2023-06-05 12:52:09 +0900

    File Reader / try - with - resource / UserException

--- 20230526_java/src/kh/lclass/io/Main.java
+++ 20230526_java/src/kh/lclass/io/Main.java
@@ -1,7 +1,18 @@
 package kh.lclass.io;
 
+import kh.lclass.exception.UserException;
+
 public class Main {
 	public static void main(String[] args) {
-		new TestFileIO().testFile();
+//		new TestFileIO().testFile();
+//		new TestFileIO().testFileRead();
+//		new TestFileIO().testFileRead2();
+		new TestFileIO().testFileRead3();
+		try {
+			new TestFileIO().testRamda();
+		} catch (UserException e) {
+			e.printStackTrace();
+		}
+		System.out.println("===main끝===");
 	}
 }
