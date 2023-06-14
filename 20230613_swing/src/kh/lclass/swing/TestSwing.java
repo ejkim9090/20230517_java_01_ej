37c2bb0d2e2c0da402e180e80ded866ea6b78c1c
ejkim9090 <ej.kh.kim@gmail.com>
2023-06-14 12:54:49 +0900

    evnet 처리 부분 수정

--- 20230613_swing/src/kh/lclass/swing/TestSwing.java
+++ 20230613_swing/src/kh/lclass/swing/TestSwing.java
@@ -35,7 +35,7 @@ public class TestSwing extends JFrame{
 		
 		// event 등록 = Action 리스너 달기
 		btnSave.addActionListener(new MyActionListener());
-		txtName.addActionListener(new MyActionListener());
+		txtName.addActionListener(new MyNameActionListener());
 		
 		// component를 contentPane에 추가
 		contentPane.setLayout(new GridLayout(5,2,10,10));
@@ -56,6 +56,24 @@ public class TestSwing extends JFrame{
 		TestSwing frame = new TestSwing();
 	}
 
+}
+
+class MyNameActionListener implements ActionListener{
+
+	public void actionPerformed(ActionEvent e) {
+		System.out.println("이건 언제 호출되지???");
+		System.out.println(e);
+		Object source  = e.getSource();
+		if(source instanceof JTextField) {
+			System.out.println("JTextField에서 enter가 눌렀네요");
+			if(((JTextField)source).getText().trim().length() == 0) {
+				((JTextField)source).setText("이름을 입력해주세요");
+			} else {
+				((JTextField)source).setText(((JTextField)source).getText().trim());
+			}
+		}
+	}
+	
 }
 
 class MyActionListener implements ActionListener{
