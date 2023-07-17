34fb6f351cd7f3e62e46829b830e5465198e96b2
ejkim9090 <ej.kh.kim@gmail.com>
2023-07-17 12:45:21 +0900

    ddl - role

--- sql/20230707_scott.sql
+++ sql/20230707_scott.sql
@@ -819,3 +819,59 @@ alter index pk_emp rebuild;
 select * from DEPT2_PUBLIC;
 select * from KH.DEPARTMENT;
 
+create sequence seq_tb1_c1 start with 10 increment by 10 maxvalue 90 minvalue 10 nocycle cache 20;
+--ORA-08002: 시퀀스 SEQ_TB1_C1.CURRVAL은 이 세션에서는 정의 되어 있지 않습니다
+--08002. 00000 -  "sequence %s.CURRVAL is not yet defined in this session"
+--*Cause:    sequence CURRVAL has been selected before sequence NEXTVAL
+--*Action:   select NEXTVAL from the sequence before selecting CURRVAL
+select seq_tb1_c1.currval from dual;
+
+select seq_tb1_c1.nextval from dual;
+select seq_tb1_c1.currval from dual;
+
+select seq_tb1_c1.nextval from dual;
+--ORA-08004: 시퀀스 SEQ_TB1_C1.NEXTVAL exceeds MAXVALUE은 사례로 될 수 없습니다
+--08004. 00000 -  "sequence %s.NEXTVAL %s %sVALUE and cannot be instantiated"
+--*Cause:    instantiating NEXTVAL would violate one of MAX/MINVALUE
+--*Action:   alter the sequence so that a new value can be requested
+
+-- ROLE
+-- 접속관련된 설정- oracle 12이후 버젼에서 false 상태로 접속됨.
+alter session set "_ORACLE_SCRIPT"=false;  
+create role role_scott_manager;
+create user kh2 ;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
