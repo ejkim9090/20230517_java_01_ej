e4531174084dc7cbb5093ecf34c95a0ec8d76928
ejkim9090 <ej.kh.kim@gmail.com>
2023-07-24 18:32:34 +0900

    주석 설명 추가

--- 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/controller/StudentGetController.java
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/controller/StudentGetController.java
@@ -32,7 +32,9 @@ public class StudentGetController extends HttpServlet {
 		// 2. 전달받은 데이터를 활용해 DB학생 상세 정보 가져오기
 		StudentDao dao =new StudentDao();
 		StudentVo vo = dao.selectOneStudent(studentNo);
+		// 3. DB로부터 전달받은 데이터를 JSP에 전달함.
 		request.setAttribute("svo", vo);
+		// 4. JSP 파일 forward로 열기
 		request.getRequestDispatcher("/WEB-INF/view/student/get.jsp").forward(request, response);
 	}
 	/**
--- 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/controller/StudentListController.java
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/student/controller/StudentListController.java
@@ -31,13 +31,14 @@ public class StudentListController extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("/student/list doGet() 진입");
-		// TODO DB
+		//// 1. 전달받은 parameter 읽어내기
+		//// 2. 전달받은 데이터를 활용해 
+		// 2. DB학생 상세 정보 가져오기
 		StudentDao dao = new StudentDao(); 
 		List<StudentVo> result = dao.selectListStudent();
+		// 3. DB로부터 전달받은 데이터를 JSP에 전달함.
 		request.setAttribute("studentList", result);
-		request.setAttribute("aaa", "그냥속성값테스트해봄");
-		request.setAttribute("bbb", "그냥속성값테스트해봄2");
-		request.setAttribute("ccc", 333);
+		// 4. JSP 파일 forward로 열기
 		request.getRequestDispatcher("/WEB-INF/view/student/list.jsp").forward(request, response);
 	}
 
