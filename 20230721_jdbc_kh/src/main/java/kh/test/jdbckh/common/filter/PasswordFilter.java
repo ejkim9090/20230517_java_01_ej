1029e6d3fd206f46a7d549b1889d50d38785a2ce
ejkim9090 <ej.kh.kim@gmail.com>
2023-07-31 12:36:09 +0900

    wrapper(pw암호일부)

--- 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/common/filter/PasswordFilter.java
+++ 20230721_jdbc_kh/src/main/java/kh/test/jdbckh/common/filter/PasswordFilter.java
@@ -7,13 +7,18 @@ import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import kh.test.jdbckh.common.filter.wrapper.PasswordWrapper;
 
 public class PasswordFilter implements Filter {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
 			throws IOException, ServletException {
-		request.getParameter("pwd");
+		HttpServletRequest httpRequest = (HttpServletRequest)request;
+		PasswordWrapper pwrapper = new PasswordWrapper(httpRequest);
+		chain.doFilter(pwrapper, response);  // request 대신 wrapper 거친 값을 넣어줌
 	}
 	
 
