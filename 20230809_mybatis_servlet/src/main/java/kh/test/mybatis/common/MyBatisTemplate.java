176d8476faaf7f6ea6f7950f4c2ce1a34ba1f3e7
ejkim9090 <ej.kh.kim@gmail.com>
2023-08-17 14:37:38 +0900

    mybatis-연습 한번더 - 완성형은 아님

--- 20230809_mybatis_servlet/src/main/java/kh/test/mybatis/common/MyBatisTemplate.java
+++ 20230809_mybatis_servlet/src/main/java/kh/test/mybatis/common/MyBatisTemplate.java
@@ -10,14 +10,13 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 public class MyBatisTemplate {
 	public static SqlSession getSqlSession() {
-		String resource = "mybatis/mybatis-config.xml";
+		//String resource = "mybatis/mybatis-config.xml";
 		SqlSession session = null;   // Connection 역할을 하는 mybatis의 개체
 		try {
 			// InputStream으로 Mybatis의 설정 정보를 읽어 온다
-			InputStream is = Resources.getResourceAsStream(resource);
+			//InputStream is = Resources.getResourceAsStream(resource);
 			// 읽어 온 Mybatis의 설정 정보를 바탕으로 SqlSessionFactoryBuilder를 생성한다
-			SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
-			SqlSessionFactory factory = builder.build(is);
+			SqlSessionFactory factory = new SqlSessionFactoryBuilder().build(Resources.getResourceAsStream("mybatis-config.xml"));
 			session = factory.openSession();
 		} catch (IOException e) {
 			e.printStackTrace();
