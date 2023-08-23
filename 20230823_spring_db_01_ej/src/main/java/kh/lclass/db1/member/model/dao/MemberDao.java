bb4cbb6b127fadf8b05910b5af28b3669770d35f
ejkim9090 <ej.kh.kim@gmail.com>
2023-08-23 17:35:42 +0900

    spring - db연동 member/list

---
+++ 20230823_spring_db_01_ej/src/main/java/kh/lclass/db1/member/model/dao/MemberDao.java
@@ -0,0 +1,40 @@
+package kh.lclass.db1.member.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Repository;
+
+import kh.lclass.db1.member.model.vo.MemberVo;
+
+@Repository("memberDao")
+public class MemberDao {
+	
+	// Autowired 사용시 Spring에서 이미 만들어진 빈객체를 가져다 씀.
+	// 따라서 = null 또는 = new Xxx() 대입하면 안됨!!!!!
+	@Autowired
+	private SqlSession sqlSession;	
+	
+	public List<MemberVo> selectList() {
+		return sqlSession.selectList("member.selectList");
+//		### Cause: java.lang.IllegalArgumentException: Mapped Statements collection does not contain value 
+//		for member.selectList]을(를) 발생시켰습니다.
+//		java.lang.IllegalArgumentException: Mapped Statements collection does not contain value for member.selectList
+		
+		
+	}
+	public MemberVo selectOne(String pk) {
+		return sqlSession.selectOne("member.selectOne", pk);
+	}
+	public int insert(MemberVo vo) {
+		return sqlSession.insert("member.insert", vo);
+	}
+	public int update(MemberVo vo) {
+		return sqlSession.update("member.update", vo);
+	}
+	public int delete(String pk) {
+		return sqlSession.delete("member.delete", pk);
+	}
+}
